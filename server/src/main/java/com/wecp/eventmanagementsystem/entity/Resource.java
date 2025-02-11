e088d54bd3930ef2fb4157f18b6c51fc09d7afe7
username <wecp-dummy@gmail.com>
2025-02-11 12:22:02 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/eventmanagementsystem/entity/Resource.java
@@ -0,0 +1,9 @@
+package com.wecp.eventmanagementsystem.entity;
+
+
+import javax.persistence.*;
+
+@Table(name = "resources") // do not change table name
+public class Resource {
+    // implement entity
+}
---
+++ server/src/main/java/com/wecp/eventmanagementsystem/service/ResourceService.java
@@ -0,0 +1,17 @@
+package com.wecp.eventmanagementsystem.service;
+
+import com.wecp.eventmanagementsystem.entity.Allocation;
+import com.wecp.eventmanagementsystem.entity.Event;
+import com.wecp.eventmanagementsystem.entity.Resource;
+import com.wecp.eventmanagementsystem.repository.EventRepository;
+import com.wecp.eventmanagementsystem.repository.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+
+public class ResourceService {
+
+}
